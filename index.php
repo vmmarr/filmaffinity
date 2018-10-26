5f43db0ca929cf21b6f1fa03d728f2b41a56f1f9
Maria del Mar Vazquez <mariadelmar.vazquez@iesdonana.org>
2018-10-26 20:53:23 +0200

    Primera version

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Bases de datos</title>
+    </head>
+    <body>
+        <?php
+        $pdo = new PDO('pgsql:host=localhost; dbname=fa', 'fa', 'fa');
+        $st = $pdo->query('select * from generos;');
+        $res = $st->fetchAll();
+        ?>
+
+        <table>
+            <thead>
+                <th>Id</th>
+                <th>Genero</th>
+            </thead>
+            <tbody>
+                <?php foreach ($res as $fila) { ?>
+                    <tr>
+                        <td><?= $fila['id'] ?></td>
+                        <td><?= $fila['genero'] ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </body>
+</html>
