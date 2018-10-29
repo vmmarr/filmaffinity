34bab4a3dd074bf0913392a206c79b28ad9840aa
Maria del Mar Vazquez <mariadelmar.vazquez@iesdonana.org>
2018-10-29 18:09:51 +0100

    Ponemos una busqueda por titulo

--- index.php
+++ index.php
@@ -6,24 +6,47 @@
     </head>
     <body>
         <?php
+        $buscarTitulo = isset($_GET['buscarTitulo']) ? trim($_GET['buscarTitulo']) : '';
         $pdo = new PDO('pgsql:host=localhost; dbname=fa', 'fa', 'fa');
-        $st = $pdo->query('SELECT * FROM generos;');
+        // $st = $pdo->query('SELECT * FROM peliculas;');
+        $st = $pdo->query("SELECT * FROM peliculas WHERE titulo ILIKE '%$buscarTitulo%'");
         // $res = $st->fetchAll(); El objeto pdo ya se puede recorrer
         ?>
 
-        <table border="1" style="margin:auto">
-            <thead>
-                <th>Id</th>
-                <th>Género</th>
-            </thead>
-            <tbody>
-                <?php foreach ($st as $fila) { ?>
-                    <tr>
-                        <td><?= $fila['id'] ?></td>
-                        <td><?= $fila['genero'] ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+        <div>
+            <fieldset>
+                <legend>Buscar...</legend>
+                <form  action="" method="get">
+                    <label for="buscarTitulo">Buscar por titulo:</label>
+                    <input type="text" id="buscarTitulo" name="buscarTitulo" value="<?= $buscarTitulo ?>">
+                    <input type="submit" value="Buscar">
+                </form>
+            </fieldset>
+        </div>
+
+        <div style="margin-top: 20px">
+            <table border="1" style="margin:auto">
+                <thead>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Año</th>
+                    <th>Sinopsis</th>
+                    <th>Duracion</th>
+                    <th>Género</th>
+                </thead>
+                <tbody>
+                    <?php foreach ($st as $fila) { ?>
+                        <tr>
+                            <td><?= $fila['id'] ?></td>
+                            <td><?= $fila['titulo'] ?></td>
+                            <td><?= $fila['anyo'] ?></td>
+                            <td><?= $fila['sinopsis'] ?></td>
+                            <td><?= $fila['duracion'] ?></td>
+                            <td><?= $fila['genero_id'] ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
