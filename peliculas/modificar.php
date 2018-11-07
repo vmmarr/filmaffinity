5d7d5100b30e7883784ae69e5d8344585131196f
Maria del Mar Vazquez <mariadelmar.vazquez@iesdonana.org>
2018-11-07 21:10:53 +0100

    Evita xSS en formulario y refactoriza

--- peliculas/modificar.php
+++ peliculas/modificar.php
@@ -2,29 +2,28 @@
 <html lang="es" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Insertar una nueva película</title>
+        <title>Modificar una pelicula</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
         <?php
         require '../comunes/auxiliar.php';
 
-        $valores = PAR;
 
         try {
             $error = [];
+            $id = comprobarId();
+            $pdo = conectar();
+            $fila = comprobarPelicula($pdo, $id);
             comprobarParametros(PAR);
             $valores = array_map('trim', $_POST);
             $flt['titulo'] = comprobarTitulo($error);
             $flt['anyo'] = comprobarAnyo($error);
             $flt['sinopsis'] = trim(filter_input(INPUT_POST, 'sinopsis'));
             $flt['duracion'] = comprobarDuracion($error);
-            $pdo = conectar();
             $flt['genero_id'] = comprobarGeneroId($pdo, $error);
             comprobarErrores($error);
-            insertarPelicula($pdo, $flt);
+            modificarPelicula($pdo, $flt, $id);
             header('Location: index.php');
         } catch (EmptyParamException|ValidationException $e) {
             // No hago nada
@@ -32,11 +31,8 @@
             header('Location: index.php');
         }
         ?>
-        <br>
         <div class="container">
-            <?php mostrarFormulario($valores, $error, 'Insertar'); ?>
+            <?php mostrarFormulario($fila, $error, 'Modificar'); ?>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
